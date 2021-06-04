8e7968a357b5049568453118120af4eeba26506d
Dina96 <>
2021-06-04 17:23:13 +0000

    adding new files

---
+++ script.js
@@ -0,0 +1,12 @@
+document.write("<p> I wish you were excited in my website " + "Dina Muasher </p>");
+
+alert("Welcome to my website")
+
+var Username= prompt("What is your name?")
+var x= "Any name"
+if(Username==x){
+  alert("Hello")
+
+}else{
+  alert("Hi")
+}
